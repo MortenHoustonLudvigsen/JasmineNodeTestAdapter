3a1a9ddfc7e7cde33d93df17a26cfa765165b732
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-03-31 12:59:29 +0200

    Create test project

---
+++ TestProjects/TypescriptTests/specs/AdderSpec.js
@@ -0,0 +1,18 @@
+var Adder = require('../src/Adder');
+describe('Adder', function () {
+    describe('add', function () {
+        // This spec should succeed:
+        describe('(3, 3)', function () {
+            it('should return 6', function () { return expect(Adder.add(3, 3)).toEqual(6); });
+        });
+        // This spec should succeed:
+        describe('(3, 6)', function () {
+            it('should return 9', function () { return expect(Adder.add(3, 6)).toEqual(6); });
+        });
+        // This spec should fail:
+        describe('(3, 7)', function () {
+            it('should return 9', function () { return expect(Adder.add(3, 7)).toEqual(6); });
+        });
+    });
+});
+//# sourceMappingURL=AdderSpec.js.map
---
+++ TestProjects/TypescriptTests/specs/AdderSpec.ts
@@ -0,0 +1,21 @@
+﻿import Adder = require('../src/Adder');
+
+describe('Adder',() => {
+    describe('add',() => {
+        // This spec should succeed:
+        describe('(3, 3)',() => {
+            it('should return 6',() => expect(Adder.add(3, 3)).toEqual(6));
+        });
+
+        // This spec should succeed:
+        describe('(3, 6)',() => {
+            it('should return 9',() => expect(Adder.add(3, 6)).toEqual(6));
+        });
+
+        // This spec should fail:
+        describe('(3, 7)',() => {
+            it('should return 9',() => expect(Adder.add(3, 7)).toEqual(6));
+        });
+    });
+});
+ 
