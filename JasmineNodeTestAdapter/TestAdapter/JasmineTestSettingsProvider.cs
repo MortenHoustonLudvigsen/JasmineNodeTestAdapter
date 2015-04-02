476f6cbfc59c2192044b543bcab6f5efa322a91f
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-04-02 14:09:33 +0200

    Add JasmineTestSettingsProvider.cs

---
+++ JasmineNodeTestAdapter/TestAdapter/JasmineTestSettingsProvider.cs
@@ -0,0 +1,20 @@
+﻿using JsTestAdapter.TestAdapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestWindow.Extensibility;
+using System.ComponentModel.Composition;
+
+namespace JasmineNodeTestAdapter.TestAdapter
+{
+    [Export(typeof(ISettingsProvider))]
+    [Export(typeof(IRunSettingsService))]
+    [Export(typeof(JasmineTestSettingsProvider))]
+    [SettingsName(JasmineTestSettings.SettingsName)]
+    public class JasmineTestSettingsProvider : TestSettingsProvider
+    {
+        protected override TestSettings CreateTestSettings()
+        {
+            return new JasmineTestSettings();
+        }
+    }
+}
