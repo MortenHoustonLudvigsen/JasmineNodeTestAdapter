b31c45dae4549ee0a6b8748f1ba24cfdd9629dc2
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-04-02 14:11:31 +0200

    Add JasmineTestRunner.cs

---
+++ JasmineNodeTestAdapter/TestAdapter/JasmineTestRunner.cs
@@ -0,0 +1,16 @@
+﻿using JsTestAdapter.TestAdapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace JasmineNodeTestAdapter.TestAdapter
+{
+    [FileExtension(".json")]
+    [DefaultExecutorUri(Globals.ExecutorUriString)]
+    [ExtensionUri(Globals.ExecutorUriString)]
+    public class JasmineTestRunner : TestRunner
+    {
+        public override TestAdapterInfo CreateTestAdapterInfo()
+        {
+            return new JasmineTestAdapterInfo();
+        }
+    }
+}
